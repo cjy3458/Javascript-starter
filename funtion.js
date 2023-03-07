20e57af13a99d6650978a36b8633a5195b77a1a8
CJY <cjy3458@naver.com>
2023-03-07 16:59:28 +0900

    Nomad

--- funtion.js
+++ funtion.js
@@ -11,3 +11,14 @@ function plus(num1, num2){
 
 plus(4, 9);
 
+
+/*내가 만든 Function & Objects */
+const player = {
+    name: "Jay",
+    sayHi: function(name) {
+        console.log("Hi~! I'm " + name);
+    }
+}
+
+console.log(player.name);
+player.sayHi(player.name);
