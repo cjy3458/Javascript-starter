2e2152f714e55da98b702a868ba5051ed0f9ce6f
CJY <cjy3458@naver.com>
2023-03-11 02:27:25 +0900

    Nomad

---
+++ cssEvent.js
@@ -0,0 +1,31 @@
+const title = document.getElementById("title");
+/*const title = document.getElementsByTagName("h1"); == 모든 tag name(h1, div, button) 접근 */
+console.dir(title);
+
+title.innerText = "Lion king";
+
+const hellos = document.getElementsByClassName("hello");
+
+const title2 = document.querySelector("div.hello:first-child h1"); /*hello 클래스 안에 첫 h1 접근*/
+
+const title3 = document.querySelectorAll(".hello h1")/*모든 h1*/
+
+
+/*그냥 요소 접근할 때는 querySelector가 제일 좋다고 생각해 */
+
+
+/*스타일 바꾸는 이벤트(보통은 css에서 -> 동작으로 스타일을 적용하기 위해 js에서 작성) */
+
+
+function handClick() {
+    const nowColor = title2.style.color;
+    let newColor;
+    if (nowColor === "blue") {
+        newColor = "rosybrown";
+    } else{
+        newColor = "blue";
+    }
+    title2.style.color = newColor;
+}
+
+title2.onclick = handClick;
--- document.js
+++ document.js
@@ -1,31 +1,26 @@
-const title = document.getElementById("title");
-/*const title = document.getElementsByTagName("h1"); == 모든 tag name(h1, div, button) 접근 */
-console.dir(title);
-
-title.innerText = "Lion king";
-
-const hellos = document.getElementsByClassName("hello");
-
-const title2 = document.querySelector("div.hello:first-child h1"); /*hello 클래스 안에 첫 h1 접근*/
-
-const title3 = document.querySelectorAll(".hello h1")/*모든 h1*/
-
-
-/*그냥 요소 접근할 때는 querySelector가 제일 좋다고 생각해 */
-
-
-/*스타일 바꾸는 이벤트 */
-
+const title = document.querySelector("div.hello:first-child h1");
 
 function handClick() {
-    const nowColor = title2.style.color;
+    const nowColor = title.style.color;
     let newColor;
     if (nowColor === "blue") {
         newColor = "rosybrown";
     } else{
         newColor = "blue";
     }
-    title2.style.color = newColor;
+    title.style.color = newColor;
 }
 
-title2.onclick = handClick;
+/*진짜 뭔말인지 하나도 모르겠네 옘병*/
+function handClick2() {
+    const active = "active";
+    title.classList.toggle("active");
+    /* if(title.classList.contains(active)){
+        title.classList.remove(active);
+    } else {
+        title.classList.add(active);   
+    }
+    */
+}
+
+title.onclick = handClick2;
