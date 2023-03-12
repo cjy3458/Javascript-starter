a37da504aa249b1c60d3a7a8d767ee94e563ead2
CJY <cjy3458@naver.com>
2023-03-12 21:20:27 +0900

    Nomad

--- Login.js
+++ Login.js
@@ -10,8 +10,7 @@ function loginSubmit(event){ /*submit이 됐을때 실행할 함수 */
     const userId = logInput.value;
     localStorage.setItem("Id", userId); /*로컬스토리지에 입력한 값을 id라는 키로 저장 */
     loginForm.classList.add("hidden"); /*form에 hidden이라는 클래스 추가 */
-    greet.innerText = `Hello ${userId}`; /*입력한 텍스트 변수에 저장 */
-    greet.classList.remove(hidden_class); /*원래있던 hidden 클래스 remove를 이용해 제거 */
+    greeting(userId);
 }
 
 loginForm.onsubmit = loginSubmit;
@@ -22,3 +21,20 @@ loginForm.onsubmit = loginSubmit;
 localStorage.setItem("Id", "Jay"); /*저장할 데이터의 key와 value 설정 */
 localStorage.getItem("Id"); /*저장된 데이터의 key를 이용해 value 접근 */
 localStorage.removeItem("Id"); /*저장된 데이터의 key를 이용해 삭제 */
+
+
+const localId = localStorage.getItem("Id");
+
+function greeting(name){
+    greet.innerText = `Hello ${name}`; /*입력한 텍스트 변수에 저장 */
+    greet.classList.remove(hidden_class); /*원래있던 hidden 클래스 remove를 이용해 제거 */
+}
+
+if(localId === null){ /*로컬스토리지에 입력한 id가 null이라면(없다면) */
+    loginForm.classList.remove(hidden_class); /*hidden이라는 클래스를 제거해서 form을 보여준다*/
+    loginForm.onsubmit = loginSubmit;
+} else {
+    greeting(localId);
+}
+
+console.log(localId);
