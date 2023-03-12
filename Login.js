24a43f39dd5b309e87f85ba8ce4c716475d53ba0
CJY <cjy3458@naver.com>
2023-03-12 18:51:14 +0900

    Nomad

--- Login.js
+++ Login.js
@@ -1,13 +1,18 @@
 const loginForm = document.querySelector(".login-form");
 const logInput = loginForm.querySelector(".login-form input");
 const loginButton = loginForm.querySelector(".login-form button");
-
-
-
+const link = document.querySelector("a");
+const greet = document.querySelector("#greeting");
+const hidden_class = "hidden";
 
 function loginSubmit(event){
     event.preventDefault();
+    const userId = logInput.value;
+    loginForm.classList.add("hidden");
+    greet.innerText = `Hello ${userId}`;
+    greet.classList.remove(hidden_class);
 }
 
+loginForm.onsubmit = loginSubmit;
 
-loginForm.submit = loginSubmit;
+
