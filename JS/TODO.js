9067a583059582f68f0cf8fb1559782d9685efbe
CJY <cjy3458@naver.com>
2023-03-15 15:55:18 +0900

    Nomad

--- JS/TODO.js
+++ JS/TODO.js
@@ -2,10 +2,19 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const todoInput = document.querySelector("#todo-form input");
 
+function deletTodo(){
+    console.log
+}
+
 function printTodo(newTodo){
     const list = document.createElement("li");
     const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌"
+    button.onclick = 
     list.appendChild(span);
+    list.appendChild(button);
     span.innerText = newTodo;
     todoList.appendChild(list);
 }
