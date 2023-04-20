d7a589395e2727c2cf550c4c2d94c4f198d939a0
CJY <cjy3458@naver.com>
2023-04-20 18:29:08 +0900

    chore: fix error

--- JS/TODO.js
+++ JS/TODO.js
@@ -3,48 +3,44 @@ const todoList = document.querySelector("#todo-list");
 const todoInput = document.querySelector("#todo-form input");
 let toDos = [];
 
-
-function saveTodo(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
-    /*현재 로컬스토리지의 저장이 안되서 새로고침하면 내 입력값이 사라짐 */
+function saveTodo() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+  /*현재 로컬스토리지의 저장이 안되서 새로고침하면 내 입력값이 사라짐 */
 }
 
-function deleteTodo(event){
-    const li = event.target.parentElement;
-    li.remove();
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
 }
 
-function printTodo(newTodo){
-    const list = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo;
-    const button = document.createElement("button");
-    button.innerText = "❌";
-    button.onclick = deleteTodo;
-    list.appendChild(span);
-    list.appendChild(button);
-    span.innerText = newTodo;
-    todoList.appendChild(list);
+function printTodo(newTodo) {
+  const list = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.onclick = deleteTodo;
+  list.appendChild(span);
+  list.appendChild(button);
+  span.innerText = newTodo;
+  todoList.appendChild(list);
 }
 
 function todoSubmit(event) {
-    event.preventDefault();
-    const newTodo = todoInput.value;
-    todoInput.value = "";
-    toDos.push(newTodo);
-    printTodo(newTodo);
-    saveTodo();
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  toDos.push(newTodo);
+  printTodo(newTodo);
+  saveTodo();
 }
 
-
 todoForm.onsubmit = todoSubmit;
-const savedTodo = localStorage.getItem("todos");
+const savedTodo = localStorage.getItem("toDos");
 
-if(savedTodo !== null){
-    const parsedTodo = JSON.parse(saveTodo);
-    toDos = parsedTodo;
-    toDos.forEach(printTodo); /*여기가 문제인거 같은데 안돼  */
-        /*array에 각 요소마다 함수를 실행시켜주는 forEach */
-
-
+if (savedTodo !== null) {
+  const parsedTodo = JSON.parse(savedTodo);
+  toDos = parsedTodo;
+  toDos.forEach(printTodo); /*여기가 문제인거 같은데 안돼  */
+  /*array에 각 요소마다 함수를 실행시켜주는 forEach */
 }
